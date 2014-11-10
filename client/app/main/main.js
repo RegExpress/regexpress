2b16126d60ecc080e1b9f53964372462d8e6be3a
Tess Myers <tess@tessmyers.com>
2014-11-10 14:29:04 -0800

    (fix) invalid regex no longer throws error

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -16,16 +16,16 @@
       $scope.match = '';
       $scope.railroad = '';
 
-      // updates regex object from input strings. TODO watch tags too
-      $scope.$watch('regexBody', function(newVal, oldVal){
+      // updates regex object from input strings, only updates if the regex is valid
+      $scope.$watchGroup(['regexBody', 'regexTags'], function(newVal, oldVal, scope){
         try {
-          $scope.regex = new RegExp(newVal);
+          scope.regex = new RegExp(newVal[0]);
         } catch (err) {
-          $scope.regex = oldVal;
+          //nobody cares about this error
         }
       })
 
-      // triggered when user clicks submit button
+      // triggered when user types in regex
       $scope.submitRegex = function(){
 
         // takes in user's regex and sets $scope.railroad to the railroad diagram for that regex
