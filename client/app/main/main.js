8bc65a49446a1a72cb9984790d5a5f84693db3b4
Tess Myers <tess@tessmyers.com>
2014-11-10 15:14:33 -0800

    live update of regex, string, matches and railroad

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -12,18 +12,37 @@
       $scope.string = '';
       $scope.regexBody = '';
       $scope.regexTags = '';
-      $scope.regex = /(?:)/;
-      $scope.match = '';
+      $scope.regex = /./;
+      $scope.matches = '';
       $scope.railroad = '';
 
       // updates regex object from input strings, only updates if the regex is valid
       $scope.$watchGroup(['regexBody', 'regexTags'], function(newVal, oldVal, scope){
+        $scope.updateRegex(newVal, oldVal);
+        $scope.updateMatch();
+      })
+
+      $scope.$watch('string', function(newVal, oldVal){
+        $scope.updateMatch();
+      })
+
+      // handles updates ( move to factory )
+      $scope.updateMatch = function(){
         try {
-          scope.regex = new RegExp(newVal[0]);
+          var tryMatch = $scope.string.match($scope.regex);
+          $scope.matches = tryMatch[0];
+        } catch (err) {
+          $scope.matches = '';
+        }
+      }
+
+      $scope.updateRegex = function(newVal, oldVal){
+        try {
+          $scope.regex = new RegExp(newVal[0], newVal[1]);
         } catch (err) {
           //nobody cares about this error
         }
-      })
+      }
 
       // triggered when user types in regex
       $scope.submitRegex = function(){
@@ -33,7 +52,6 @@
         $scope.railroad = $sce.trustAsHtml(newRR.toString());
 
         // update match string through test-string directive TODO
-
 
       }
 
