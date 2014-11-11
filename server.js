5713748a529eb3de6d0d79b31f15b5ee0fdbf716
Tyler Garrett <tyler@tgrrtt.com>
2014-11-10 16:22:30 -0800

    Renamed tempServer to server & changed package.json

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var server = express();
 
 
-var port = 3000;
+var port = 8000;
 
 server.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, '/client/index.html'));
