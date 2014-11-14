33cc5360ad236366f5c790930e085b04219b8157
Tyler Garrett <tyler@tgrrtt.com>
2014-11-14 11:19:05 -0800

    Added phantomjs for karma testing

--- karma.conf.js
+++ karma.conf.js
@@ -1,38 +1,22 @@
-module.exports = function(config){
+module.exports = function(config) {
   config.set({
-
+    browsers: ['PhantomJS'],
     basePath : './',
 
+    plugins : [
+            'karma-phantomjs-launcher',
+            'karma-jasmine'
+            ],
     files : [
       // be careful about globbing, make sure that the stuff you need first is globbed first
       'client/bower_components/angular/angular.js',
       'client/bower_components/angular-route/angular-route.js',
       'client/bower_components/angular-mocks/angular-mocks.js',
-
       'client/app/main/main.js',
-
       'client/components/**/*.directive.js',
       'client/components/**/*.specs.js'
-
     ],
-
     autoWatch : true,
-
     frameworks: ['jasmine'],
-
-    browsers : ['Chrome'],
-
-    plugins : [
-            'karma-chrome-launcher',
-            'karma-firefox-launcher',
-            'karma-jasmine',
-            'karma-junit-reporter'
-            ],
-
-    junitReporter : {
-      outputFile: 'test_out/unit.xml',
-      suite: 'unit'
-    }
-
   });
 };
