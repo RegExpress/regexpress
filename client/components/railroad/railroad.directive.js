2fe1dc828b76d2d3a64641b48c653ef9aed9a3c1
Tess Myers <tess@tessmyers.com>
2014-11-17 16:04:17 -0800

    drop on RR diagram reverts the diagram, drop off results in a removal of the node

--- client/components/railroad/railroad.directive.js
+++ client/components/railroad/railroad.directive.js
@@ -19,59 +19,67 @@
         scope.$watch('main.regexp', function(newVal, oldVal, scope){
           scope.main.regexTree = parseRegex(scope.main.regexp);
           scope.main.treeChanged++;
-
-          // this below is just for testing purposes.
-          window.regexpTree = scope.main.regexTree;
         });
 
         // watches tree counter, which is tripped everytime the tree is DONE changing.
         // Creates and appends new Railroad diagram
         scope.$watch('main.treeChanged', function(newVal, oldVal, scope){
-          console.log("treeCounter changes", scope.main.treeChanged);
           var newRR = scope.rr.createRailroad(scope.main.regexTree);
           element.empty();
           element.append( '<div>'+ newRR+'</div>');
         });
-//AAAAAAAAAAAAAAAAAAAAAAAAAA
+
         // set selected item and itemID
         element.on('mousedown',function(event){
           if (event.which === 1) {
-            item = $(event.toElement).closest('.literal-sequence, .capture-group, .charset');
+            item = $(event.toElement).closest('.literal-sequence, .capture-group, .charset')
             itemID = item.attr('id');
-            console.log($(item).closest('g')); 
-          }
+            // console.log($(item).closest('g'))
 
-          // if (event.which === 1) {
-          //   // create clone and append to DOM
-          //   copy = $(item).clone()
-          //     .attr('fill', 'black')
-          //     .wrap('<svg class="copy" style="position: absolute;"></svg>')
-          //     .parent();
+            if (event.which === 1) {
+              // create clone and append to DOM
+              copy = $(item).clone()
+                .attr('fill', 'black')
+                .attr('draggable', true)
+                .wrap('<svg class="copy" style="position: absolute;"></svg>')
+                .parent();
 
-          //   $(copy).css({
-          //     top: 20,
-          //     left: 20
-          //   })
+              $('.work').append(copy);
 
-          //   $('.work').append(copy);
+              $(copy).css({
+                top: event.pageY - 220,
+                left: event.pageX -20
+              })
 
-          //   // gray out the selected item
-          //   oldClass = $(item).attr('class');
-          //   $(item).attr('class','ghost '+ oldClass);
-          //   $('g.ghost rect').css('stroke', 'gray');
-          // } else if (event.which === 3) {
-          //   console.log($(item))
-          // }
+              $(copy).bind('drag', function(event){
+                console.log('DRAGG!')
+              })
+
+              // gray out the selected item
+              oldClass = $(item).attr('class');
+              $(item).attr('class','ghost '+ oldClass);
+              $('g.ghost rect').css('stroke', 'gray');
+            } else if (event.which === 3) {
+              console.log($(item))
+            }
         })
 
         // removed item from tree on mouseup if off the RR
         $('body').on('mouseup', function(event) {
-          if (handlerHelpers.checkLocation(event) != 'svg') {
+
+          if (handlerHelpers.checkUnder(event) === 'RR-dir') {
+            console.log('fail', handlerHelpers.checkUnder(event))
+
             var intID = parseInt(itemID)
-            modifyTree.removeNode(intID, scope.main.regexTree);
+
+            try {
+              modifyTree.removeNode(intID, scope.main.regexTree);
+            } catch (err) { console.log('error caught', err); }
+
             scope.$apply(function(){
               scope.main.treeChanged++;
             });
+
           } else {
             // un-gray the dropped item
             $('g.ghost rect').css('stroke', 'black');
@@ -79,17 +87,19 @@
           }
 
           item = undefined;
+          console.log('removing copy');
           $(copy).remove();
         });
 
-        // $('body').on('mousemove', function(event){
-        //   if (item) {
-        //     $(copy).css({
-        //       top: event.pageY,
-        //       left: event.pageX
-        //      })
-        //   }
-        // })
+        $('body').on('mousemove', function(event){
+          if (item) {
+            $(copy).css({
+              top: event.pageY -220,
+              left: event.pageX - 20
+             })
+          }
+        })
+
       }
     }
   }
