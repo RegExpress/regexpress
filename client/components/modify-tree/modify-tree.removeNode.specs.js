3940a768e1d1ab9e815a8ffca21b7cf53a1be376
Mike <mikeh09@gmail.com>
2014-11-19 11:13:04 -0800

    (refactor) moved modify tree tests into separate files

--- client/components/modify-tree/modify-tree.removeNode.specs.js
+++ client/components/modify-tree/modify-tree.removeNode.specs.js
@@ -1,4 +1,4 @@
-describe('Modify-Tree\n', function() {
+describe('Modify-Tree-removeNode\n', function() {
   var $rootScope, $scope, modifyTree, tree1, tree2, tree3, tree4, tree5;
   beforeEach(module('baseApp')); // the specific module
   beforeEach(inject(function($injector) {
@@ -20,8 +20,7 @@ describe('Modify-Tree\n', function() {
   });
 
   describe('modifyTree has the right functions\n', function(){
-    it('should have addNode and removeNode functions', function(){
-      expect(modifyTree.addNode).toEqual(jasmine.any(Function));
+    it('should have a removeNode function', function(){
       expect(modifyTree.removeNode).toEqual(jasmine.any(Function));
     });
   });
@@ -101,33 +100,8 @@ describe('Modify-Tree\n', function() {
         modifyTree.removeNode(8, tree3);
         modifyTree.removeNode(11, tree3);
         expect(JSON.stringify(tree3)).toEqual('{"type":"match","offset":0,"text":"(a|b|c)","body":[{"type":"capture-group","offset":1,"text":"a|b|c","body":{"type":"match","offset":5,"text":"c","body":[{"type":"literal","offset":5,"text":"c","body":"c","escaped":false,"idNum":13}],"idNum":12},"index":1,"idNum":5}],"idNum":4}')
-      })
-
-    });
-  });
-
-  describe('addNode adds nodes to the tree\n', function(){
-    describe('adds a node to a match\n', function(){
-      it('adds a literal at the front of a match', function(){
-        modifyTree.addNode(null, 4, {"type":"literal","body":"z"}, tree1);
-        expect(JSON.stringify(tree1)).toEqual('{"type":"match","offset":0,"text":"ab?cd?e","body":[{"type":"literal","body":"z"},{"type":"literal","offset":0,"text":"a","body":"a","escaped":false,"idNum":5},{"type":"quantified","offset":1,"text":"b?","body":{"type":"literal","offset":1,"text":"b","body":"b","escaped":false,"idNum":7},"quantifier":{"type":"quantifier","offset":2,"text":"?","min":0,"max":1,"greedy":true},"idNum":6},{"type":"literal","offset":3,"text":"c","body":"c","escaped":false,"idNum":8},{"type":"quantified","offset":4,"text":"d?","body":{"type":"literal","offset":4,"text":"d","body":"d","escaped":false,"idNum":10},"quantifier":{"type":"quantifier","offset":5,"text":"?","min":0,"max":1,"greedy":true},"idNum":9},{"type":"literal","offset":6,"text":"e","body":"e","escaped":false,"idNum":11}],"idNum":4}');
       });
 
-      it('adds a literal at the end of a match', function(){
-        modifyTree.addNode(11, 4, {"type":"literal","body":"z"}, tree1);
-        expect(JSON.stringify(tree1)).toEqual('{"type":"match","offset":0,"text":"ab?cd?e","body":[{"type":"literal","offset":0,"text":"a","body":"a","escaped":false,"idNum":5},{"type":"quantified","offset":1,"text":"b?","body":{"type":"literal","offset":1,"text":"b","body":"b","escaped":false,"idNum":7},"quantifier":{"type":"quantifier","offset":2,"text":"?","min":0,"max":1,"greedy":true},"idNum":6},{"type":"literal","offset":3,"text":"c","body":"c","escaped":false,"idNum":8},{"type":"quantified","offset":4,"text":"d?","body":{"type":"literal","offset":4,"text":"d","body":"d","escaped":false,"idNum":10},"quantifier":{"type":"quantifier","offset":5,"text":"?","min":0,"max":1,"greedy":true},"idNum":9},{"type":"literal","offset":6,"text":"e","body":"e","escaped":false,"idNum":11},{"type":"literal","body":"z"}],"idNum":4}');
-      });
-
-      it('adds a literal in the middle of a match', function(){
-        modifyTree.addNode(8, 4, {"type":"literal","body":"z"}, tree1);
-        expect(JSON.stringify(tree1)).toEqual('{"type":"match","offset":0,"text":"ab?cd?e","body":[{"type":"literal","offset":0,"text":"a","body":"a","escaped":false,"idNum":5},{"type":"quantified","offset":1,"text":"b?","body":{"type":"literal","offset":1,"text":"b","body":"b","escaped":false,"idNum":7},"quantifier":{"type":"quantifier","offset":2,"text":"?","min":0,"max":1,"greedy":true},"idNum":6},{"type":"literal","offset":3,"text":"c","body":"c","escaped":false,"idNum":8},{"type":"literal","body":"z"},{"type":"quantified","offset":4,"text":"d?","body":{"type":"literal","offset":4,"text":"d","body":"d","escaped":false,"idNum":10},"quantifier":{"type":"quantifier","offset":5,"text":"?","min":0,"max":1,"greedy":true},"idNum":9},{"type":"literal","offset":6,"text":"e","body":"e","escaped":false,"idNum":11}],"idNum":4}');
-      });
-    });
-    describe('adds a node to an alternate\n', function(){
-      it('adds a literal to the end of an alternate', function(){
-        modifyTree.addNode(null, 6, {"type":"match","body":[{"type":"literal","body":"d"}]}, tree3);
-        expect(JSON.stringify(tree3)).toEqual('{"type":"match","offset":0,"text":"(a|b|c)","body":[{"type":"capture-group","offset":1,"text":"a|b|c","body":{"type":"alternate","offset":1,"text":"a|b|c","left":{"type":"match","offset":1,"text":"a","body":[{"type":"literal","offset":1,"text":"a","body":"a","escaped":false,"idNum":8}],"idNum":7},"right":{"type":"alternate","offset":3,"text":"b|c","left":{"type":"match","offset":3,"text":"b","body":[{"type":"literal","offset":3,"text":"b","body":"b","escaped":false,"idNum":11}],"idNum":10},"right":{"type":"alternate","left":{"type":"match","offset":5,"text":"c","body":[{"type":"literal","offset":5,"text":"c","body":"c","escaped":false,"idNum":13}],"idNum":12},"right":{"type":"match","body":[{"type":"literal","body":"d"}]}},"idNum":9},"idNum":6},"index":1,"idNum":5}],"idNum":4}');
-      });
     });
   });
 
