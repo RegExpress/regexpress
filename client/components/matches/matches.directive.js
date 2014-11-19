43e588a99358cdd41814f91283b9d15c024f9c6c
Shan Swami <shan.swami@gmail.com>
2014-11-18 19:54:28 -0800

    cleans up files, removes dependencies

--- client/components/matches/matches.directive.js
+++ client/components/matches/matches.directive.js
@@ -12,29 +12,22 @@
       template: '<div>matches</div>',
       link: function(scope, element, attrs) {
         // watches for changes in regex, creates matched string and appends to DOM
-        scope.$watch('main.regexp', function(newVal, oldVal){
+        scope.$watchGroup(['main.regexp', 'main.string'], function(newVal, oldVal){
           try {
             scope.main.matches = scope.main.string.match(scope.main.regexp);
             var stuff = scope.main.string.match(scope.main.regexp);
             element.empty();
             element.append('<p>' + scope.main.matches + '</p>');
-            console.log(stuff)
-            //
-            // $scope.highlight = function(text, search) {
-            //   if (!search) {
-            //     return text;
-            //   }
-            //   console.log("this is being called")
-            //   return text.replace(new RegExp(search, 'gi'), '<span class="highlightedText">$&</span>');
-            // };
-            // $scope.highlight(scope.main.string, scope.main.matches);
+            console.log(stuff, "stuff")
+            var wordMatches = stuff[0].split(' ')
+            console.log(wordMatches)
+            $('#textarea').highlightTextarea({
+              words: ['{wordMatches}']
+            })
+            console.log("are we gettting here")
           } catch (err) {
-            // triggered when there are no matches
-            // console.log(err)
+            console.log(err)
           }
-
-
-
         });
       }
     }
