98764ebfb7608d0b0c573819d28cbd17f686e4a2
Mike <mikeh09@gmail.com>
2014-11-18 17:06:09 -0800

    (test) checks if addNode and removeNode functions exist

--- client/components/modify-tree/modify-tree.specs.js
+++ client/components/modify-tree/modify-tree.specs.js
@@ -1,32 +1,25 @@
 describe('Modify-Tree', function() {
-  var $rootScope, $scope;
+  var $rootScope, $scope, Auth;
   beforeEach(module('baseApp')); // the specific module
   beforeEach(inject(function($injector) {
 
     $rootScope = $injector.get('$rootScope');
     $scope = $rootScope.$new();
+    modifyTree = $injector.get('modifyTree')
     var $controller = $injector.get('$controller');
-
-    createController = function() {
-      return $controller('MainController', {
-        $scope: $scope
-      });
-    };
-
-    createController();
 
   }));
 
   describe('universe is ok', function(){
     it('should not have broken reality', function(){
       expect(true).toBe(true);
-      expect($scope.callAdd).toEqual(jasmine.any(Function));
     });
   });
 
-  describe('scope has the right stuff on it', function(){
-    it('should have a re attr', function(){
-      
+  describe('modifyTree has the right functions', function(){
+    it('should have addNode and removeNode functions', function(){
+      expect(modifyTree.addNode).toEqual(jasmine.any(Function));
+      expect(modifyTree.removeNode).toEqual(jasmine.any(Function));
     });
   });
 
