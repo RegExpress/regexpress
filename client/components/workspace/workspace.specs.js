3b3f4e2bf638243031baccd221a33ea8b068ab20
Mike <mikeh09@gmail.com>
2014-11-20 15:27:39 -0800

    (test) tests some special types for getComponentNode

--- client/components/workspace/workspace.specs.js
+++ client/components/workspace/workspace.specs.js
@@ -19,4 +19,41 @@ describe('Workspace', function() {
     });
   });
 
-})
+  describe('getComponentNode returns the correct nodes\n', function(){
+    describe('handles non-terminal types\n', function(){
+      it('should support start and end objects', function(){
+        var start = workspace.getComponentNode('start');
+        expect(JSON.stringify(start)).toEqual('{"type":"start"}');
+        var end = workspace.getComponentNode('end');
+        expect(JSON.stringify(end)).toEqual('{"type":"start"}');
+      });
+
+      it('should support any-character objects', function(){
+        var anyChar = workspace.getComponentNode('any-character');
+        expect(JSON.stringify(anyChar)).toEqual('{"type":"any-character"}');
+      });
+
+      it('should support word and non-word objects', function(){
+        var word = workspace.getComponentNode('word');
+        expect(JSON.stringify(word)).toEqual('{"type":"word"}');
+        var nonword = workspace.getComponentNode('non-word');
+        expect(JSON.stringify(non-word)).toEqual('{"type":"non-word"}');
+      });
+
+      it('should support white-space and non-white-space objects', function(){
+        var whitespace = workspace.getComponentNode('white-space');
+        expect(JSON.stringify(whitespace)).toEqual('{"type":"white-space"}');
+        var nonwhitespace = workspace.getComponentNode('non-white-space');
+        expect(JSON.stringify(nonwhitespace)).toEqual('{"type":"non-white-space"}');
+      });
+
+      it('should support digit and non-digit objects', function(){
+        var digit = workspace.getComponentNode('digit');
+        expect(JSON.stringify(digit)).toEqual('{"type":"digit"}');
+        var nondigit = workspace.getComponentNode('non-digit');
+        expect(JSON.stringify(nondigit)).toEqual('{"type":"non-digit"}');
+      });
+
+    });
+  });
+});
