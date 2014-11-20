9c9b6eb7a5dabd925be1f1f8b2485fc5a38ed3a3
Mike <mikeh09@gmail.com>
2014-11-20 14:27:00 -0800

    (feat) added workspace factory file

---
+++ client/components/workspace/workspace.factory.js
@@ -0,0 +1,13 @@
+(function() {
+  'use strict';
+
+  angular.module('workspaceModule', [])
+    .factory('workspace', modifyTree);
+
+    function workspace(){
+
+    	return {
+
+    	};
+    }
+})();
