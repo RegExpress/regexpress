752b1cd112b5af92867e5c4da49fed51b4e6e00e
Mike <mikeh09@gmail.com>
2014-11-20 16:42:09 -0800

    (feat) getComponentNode supports a bunch of non-literal types

--- client/components/workspace/workspace.factory.js
+++ client/components/workspace/workspace.factory.js
@@ -7,11 +7,19 @@
     function workspace(){
 
       var componentTable = {
-        
+        'start': {'type': 'start'},
+        'end': {'type': 'end'},
+        'any-character': {'type': 'any-character'},
+        'word': {'type': 'word'},
+        'non-word': {'type': 'non-word'},
+        'digit': {'type': 'digit'},
+        'non-digit': {'type': 'non-digit'},
+        'white-space': {'type': 'white-space'},
+        'non-white-space': {'type': 'non-white-space'}
       };
 
       function getComponentNode(type){
-
+        return componentTable[type];
       }
 
     	return {
