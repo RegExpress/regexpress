2767a0d2ff1e77304e628f56d6e3443f4146fed4
Mike <mikeh09@gmail.com>
2014-11-20 17:02:06 -0800

    (feat) getComponentNode handles text, capture-group, and some choice blocks

--- client/components/workspace/workspace.factory.js
+++ client/components/workspace/workspace.factory.js
@@ -6,6 +6,8 @@
 
     function workspace(){
 
+      var text = {'type': 'match', 'body': [{'type': 'literal', 'body': 'a'}, {'type': 'literal', 'body': 'b'}, {'type': 'literal', 'body': 'c'}]} 
+
       var componentTable = {
         'start': {'type': 'start'},
         'end': {'type': 'end'},
@@ -15,7 +17,12 @@
         'digit': {'type': 'digit'},
         'non-digit': {'type': 'non-digit'},
         'white-space': {'type': 'white-space'},
-        'non-white-space': {'type': 'non-white-space'}
+        'non-white-space': {'type': 'non-white-space'},
+        'text': text,
+        'capture-group': {'type': 'capture-group', 'body': text},
+        'alternate': {'type': 'alternate', 'left': text, 'right': text},
+        'optional': {'type': 'quantified', 'body': text, 'quantifier': {'min': 0, 'max': 1}},
+        'repeating': {'type': 'quantified', 'body': text, 'quantifier': {'min': 1, 'max': Infinity}}
       };
 
       function getComponentNode(type){
