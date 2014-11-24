71908c49de408b004ad219b8c931f2d5d1778d49
Tess Myers <tess@tessmyers.com>
2014-11-24 15:52:22 -0800

    adds drag and drop in component library

--- client/components/workspace/workspace.directive.js
+++ client/components/workspace/workspace.directive.js
@@ -28,6 +28,29 @@
           scope.main.nodeToAdd = undefined;
         })
 
+        $(element).on('mousedown', function(event){
+          var clone = $(event.target).clone()
+            .attr('class', 'drag components')
+            .css({
+              position: 'absolute',
+              top: event.pageY+5,
+              left: event.pageX+5
+            });
+
+          $('.container').append(clone);
+        })
+
+        $('body').on('mousemove', function(){
+          $('.drag').css({
+              top: event.pageY+5,
+              left: event.pageX+5
+            })
+        })
+
+        $('body').on('mouseup', function(){
+          $('.drag').remove();
+        })
+
       }
     }
   }
