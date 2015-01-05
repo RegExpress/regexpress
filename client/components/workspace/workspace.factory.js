fbbcd91d28df4e0e621cc412ac004ab1b40b5ff8
Tyler Garrett <tyler@tgrrtt.com>
2015-01-04 18:27:25 -0800

    Revert "changes structure of alternate component to not be in a goddamn capture group"
    This reverts commit 968459882b0223c2d3699a0a457c461773077924.

--- client/components/workspace/workspace.factory.js
+++ client/components/workspace/workspace.factory.js
@@ -29,7 +29,7 @@
         'text': text,
         // 'testText': testText,
         'capture-group': '{"type": "capture-group", "body":' + text + '}',
-        'alternate': '{"type": "alternate", "left":' + text + ', "right":' + text + '}',
+        'alternate': '{"type": "capture-group", "body": {"type": "alternate", "left":' + text + ', "right":' + text + '}}',
         'optional': '{"type": "quantified", "body": {"type": "capture-group", "body":' + text + '}, "quantifier": {"min": 0, "max": 1}}',
         'repeating': '{"type": "quantified", "body": {"type": "capture-group", "body":' + text + '}, "quantifier": {"min": 1, "max": null}}'
       };
