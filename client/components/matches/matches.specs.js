41ca14e311a71549d9e6266e3d778e70fbaa3d5f
Tess Myers <tess@tessmyers.com>
2015-01-11 20:11:21 -0800

    Adds tests for string matching html generator module

---
+++ client/components/matches/matches.specs.js
@@ -0,0 +1,36 @@
+describe('matches', function() {
+  var $rootScope, $scope, makeMatches;
+  beforeEach(module('baseApp')); // the specific module
+  beforeEach(inject(function($injector) {
+
+    makeMatches = $injector.get('makeMatches');
+
+  }));
+
+  describe('universe is ok', function(){
+    it('should not have broken reality', function(){
+      expect(true).toBe(true);
+    });
+  });
+
+  describe('makeMatches has the right functions', function(){
+    it('should have a getMatchHTML function', function(){
+      expect(makeMatches.getMatchHTML).toEqual(jasmine.any(Function));
+    });
+  });
+
+  describe('getMatchHTML should work properly', function(){
+    it('should return a string', function(){
+      expect(makeMatches.getMatchHTML('abc 123', /\d+/)).toEqual(jasmine.any(String));
+    });
+
+    it('should handle a single match', function(){
+      expect(makeMatches.getMatchHTML('abc 123', /\d+/)).toBe('abc <span class="hilight">123</span>');
+    });
+
+    it('should handle more than one match', function(){
+      expect(makeMatches.getMatchHTML('abc 123 efg 456', /\d+/g)).toBe('abc <span class="hilight">123</span> efg <span class="hilight">456</span>');
+    });
+  });
+
+});
